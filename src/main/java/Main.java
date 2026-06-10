0c166b108b58010dfc6a2ee67bba5371798d587b
vibhanshu-agarwal <vibhanshu.agarwal@gmail.com>
2026-06-10 15:33:53 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,9 +19,19 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            //Write PONG to the outputstream of the client socket
-            //remember to start with +, because in RESP(Redis Serialization Protocol) protocol, + indicates a simple string response
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            //Read PING commands from the client socket input stream until the client closes the connection
+            InputStream inputStream = clientSocket.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                String command = new String(buffer, 0, bytesRead).trim();
+                // If the command is PING, respond with PONG
+                if (command.toUpperCase().contains("PING")) {
+                    //Write PONG to the outputstream of the client socket
+                    //remember to start with +, because in RESP(Redis Serialization Protocol) protocol, + indicates a simple string response
+                    clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+                }
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
