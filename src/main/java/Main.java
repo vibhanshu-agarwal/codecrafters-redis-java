65297d15dd594fa03d07013934df532dd7871e5f
vibhanshu-agarwal <vibhanshu.agarwal@gmail.com>
2026-06-10 15:54:55 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,39 +10,43 @@ public class Main {
         System.out.println("Logs from your program will appear here!");
 
         //  Uncomment the code below to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread.startVirtualThread(() -> handleClient(clientSocket));
+            }
+        } catch (IOException e) {
+            //Add a sout
+            System.out.println("Server error: " + e.getMessage());
+        }
+    }
+
+    private static void handleClient(Socket clientSocket) {
+        //Encapsulate code in try/resources to ensure that resources are properly closed
+        try (clientSocket; InputStream inputStream = clientSocket.getInputStream()) {
             //Read PING commands from the client socket input stream until the client closes the connection
-            InputStream inputStream = clientSocket.getInputStream();
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 String command = new String(buffer, 0, bytesRead).trim();
                 // If the command is PING, respond with PONG
                 if (command.toUpperCase().contains("PING")) {
-                    //Write PONG to the outputstream of the client socket
+                    //Write PONG to the OutputStream of the client socket
                     //remember to start with +, because in RESP(Redis Serialization Protocol) protocol, + indicates a simple string response
                     clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
                 }
             }
+
         } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
+            // Handle exceptions that may occur while reading from the client socket
+            System.out.println("Error handling client: " + e.getMessage());
         }
+
     }
 }
