0fea5336d3b0c13dbdaa42b516c97a8da5b406b4
vibhanshu-agarwal <vibhanshu.agarwal@gmail.com>
2026-06-10 15:09:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,8 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+            //Write PONG to the outputstream of the client socket
+            clientSocket.getOutputStream().write("PONG\r\n".getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
